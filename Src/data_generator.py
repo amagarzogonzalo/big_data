22a7a8f4d2e5aa08635e692d22c38917ac7cfb81
amagarzogonzalo <amagarzogonzalo@gmail.com>
2024-06-23 19:30:38 +0200

    time  + num process + distinct process

--- Src/data_generator.py
+++ Src/data_generator.py
@@ -1,6 +1,7 @@
 import random
 from states import user, str_to_class
 import json
+import os
 
 class Timer:
     def __init__ (self, time):
@@ -32,32 +33,59 @@ def iterate_state(prev_state, subtasks, Timer, process_id):
         subtasks.append([state,prev_state, "Response", Timer.curr_time, process_id])
     
 
-def create_data(N=1000):
+def create_data(namefile = "data_processes.json",distinct_process = 10, num_process=2):
     global_time = 0
-    subtasks = []
+    aux_processes, processes = [], []
 
-    for i in range(N):
-        subtasks_i = []
+
+    for i in range(distinct_process):
+        processs_i = []
         prev_state = user
 
         process_id = f"process{i+1}"
         timer = Timer(global_time)
-        iterate_state(prev_state, subtasks_i, timer, process_id)
-        subtasks += subtasks_i
+        iterate_state(prev_state, processs_i, timer, process_id)
+        #print("proce    ", processs_i)
+        aux_processes.append(processs_i)
 
         global_time+=random.randint(0,1000)
 
-    serializable_subtasks = []
-    for subtask in subtasks:
-        state_from, state_to, action, time, process_id = subtask
-        subtask_dict = {
-            'state_from': state_from.__name__ if hasattr(state_from, '__name__') else state_from,
-            'state_to': state_to.__name__ if hasattr(state_to, '__name__') else state_to,
-            'action': action,'time': time,'process_id': process_id
-        }
-        serializable_subtasks.append(subtask_dict)
 
-    with open("Data/data_processes.json", 'w') as f:
+    real_time = 0
+
+    for i in range(num_process):
+        processs_i = []
+
+        timer = Timer(real_time)
+
+        process_i = random.choice(aux_processes)
+        process_i = [[real_time + j * random.randint(100,110) if isinstance(process_i[j][i], (int, float)) else process_i[j][i] for i in range(len(process_i[j]))] for j in range(len(process_i))]
+
+
+        print(process_i)
+        real_time+=100
+        processes.append( process_i)
+
+
+            
+
+
+
+    
+    serializable_subtasks = []
+    for p in processes:
+        for subtask in p:
+            state_from, state_to, action, time, process_id = subtask
+            subtask_dict = {
+                'state_from': state_from.__name__ if hasattr(state_from, '__name__') else state_from,
+                'state_to': state_to.__name__ if hasattr(state_to, '__name__') else state_to,
+                'action': action,'time': time,'process_id': process_id
+            }
+            serializable_subtasks.append(subtask_dict)
+
+
+    path = os.path.join(os.path.dirname(os.getcwd()), "Data", namefile)
+    with open(path, 'w') as f:
         json.dump(serializable_subtasks, f, indent="")
 
 
